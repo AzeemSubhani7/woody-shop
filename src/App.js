4ff0f64adc4aea4588e93fcdd423961f9515c27d
Azeem Subhani <azeemforall4406148@gmail.com>
2021-01-31 14:31:48 +0500

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 
 function App() {
-  return (
-      <div className="App">
-        <h1>Hello Azeem!</h1>
-      </div>
-  );
+    return (
+        <div>
+            <h1>Hello Azeem nd World!</h1>
+        </div>
+    );
 }
+
 
 export default App;
