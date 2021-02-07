f1bfce39ddeb0582a0407197adef2cdf4fe1a484
Azeem Subhani <azeemforall4406148@gmail.com>
2021-02-07 19:20:56 +0500

    Adding header component and links to Shop page and Contacts

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import HomePage from "./components/homepage";
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import HatsPage from './components/page/hatspage';
 import ShopPage from './components/page/shoppage';
+import Header from './components/header'
 
 function App() {
     return (
         <div>
         <Router>
+            <Header />
             <Switch>
                 <Route exact path="/" component={HomePage}/>
                 <Route path='/hats' component={HatsPage}/>
---
+++ src/components/header.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import { ReactComponent as ReactLogo } from '../assets/crown.svg';
+//import { default as logo } from '../assets/crown.svg';
+
+import './css/header.scss';
+
+// <link to='/' className='logo-container'>
+//         <ReactLogo className='logo' />
+//     </link>
+//     <div className="options">
+//         <Link to='/shop' className='option'>
+//         SHOP
+//         </Link>
+//         <Link to='/contact' className='option'>
+//         CONTACT
+//         </Link>
+//     </div>
+
+
+
+const Header = () => (
+        <div className='header'>
+            <Link to='/' className='logo-container'>
+                <ReactLogo className='logo'/>
+            </Link>
+            <div className="options">
+                <Link to='/shop' className='option'>
+                <h3>SHOP</h3>
+                </Link>
+                <Link to='/shop' className='option'>
+                <h3>CONTACT</h3>
+                </Link>
+            </div>
+        </div>
+    );
+
+
+export default Header;
